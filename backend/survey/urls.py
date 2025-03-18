a671d015b5917b335d8f516c312bf6460282c2f6
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-03-18 13:30:08 +0300

    login view

--- backend/survey/urls.py
+++ backend/survey/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SubmitSurveyView, SurveyResultsView
+from .views import SubmitSurveyView, SurveyResultsView, AdminLoginView
 
 urlpatterns = [
     path('api/survey/', SubmitSurveyView.as_view(), name='submit_survey'),
     path('api/results/', SurveyResultsView.as_view(), name='survey_results'),
+    path('api/login/', AdminLoginView.as_view(), name='admin-login'),
 ]
--- backend/survey/views.py
+++ backend/survey/views.py
@@ -1,11 +1,24 @@
+from django.contrib.auth.views import LoginView
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import SurveyResponse
 from .serializers import SurveyResponseSerializer
+
 
 class SubmitSurveyView(generics.CreateAPIView):
     queryset = SurveyResponse.objects.all()
     serializer_class = SurveyResponseSerializer
 
+
 class SurveyResultsView(generics.ListAPIView):
     queryset = SurveyResponse.objects.prefetch_related('benefits').all()
     serializer_class = SurveyResponseSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+
+class AdminLoginView(LoginView):
+    template_name = "login.html"
+
+    def get_success_url(self):
+        """После успешного входа отправляет на API /api/results/"""
+        return "/api/results/"
